61df74e1396aac11e6fede9bb741c1cc4e96b9d9
sahalkk <msahalkk357@gmail.com>
2023-08-25 00:00:16 +0530

    added functions

--- src/App.js
+++ src/App.js
@@ -19,30 +19,59 @@ function App() {
     setValues({ ...values, email: event.target.value });
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (values.firstName && values.lastName && values.email) {
+      setValid(true);
+    }
+    setSubmitted(true);
+  };
+
+  const [submitted, setSubmitted] = useState(false);
+  const [valid, setValid] = useState(false);
+
   return (
     <div>
-      <form className="form">
-        <input
-          onChange={handleFirstNameInputChange}
-          value={values.firstName}
-          className="form-field"
-          type="text"
-          placeholder="First Name"
-        ></input>
-        <input
-          onChange={handleLastNameInputChange}
-          value={values.lastName}
-          className="form-field"
-          type="text"
-          placeholder="Last Name"
-        ></input>
-        <input
-          onChange={handleEmailInputChange}
-          value={values.email}
-          className="form-field"
-          type="email"
-          placeholder="Email"
-        ></input>
+      <form className="form" onSubmit={handleSubmit}>
+        {submitted && valid ? (
+          <div className="success-message">
+            Success! Thank you for registering
+          </div>
+        ) : null}
+        <div className="wrapper">
+          <input
+            onChange={handleFirstNameInputChange}
+            value={values.firstName}
+            className="form-field"
+            type="text"
+            placeholder="First Name"
+          ></input>
+          {submitted && !values.firstName ? (
+            <span>Please enter a name</span>
+          ) : null}
+
+          <input
+            onChange={handleLastNameInputChange}
+            value={values.lastName}
+            className="form-field"
+            type="text"
+            placeholder="Last Name"
+          ></input>
+          {submitted && !values.lastName ? (
+            <span>Please enter a name</span>
+          ) : null}
+
+          <input
+            onChange={handleEmailInputChange}
+            value={values.email}
+            className="form-field"
+            type="email"
+            placeholder="Email"
+          ></input>
+          {submitted && !values.email ? (
+            <span>Please enter a email address</span>
+          ) : null}
+        </div>
         <button className="button">Register</button>
       </form>
     </div>
